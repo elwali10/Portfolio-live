302763cf3f26a52d0f2591573cf3467a456f6b12
Elwali <elwali@Wali.K>
2021-01-01 21:10:50 +0100

    media add

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -126,7 +126,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [ 
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media')
+    
     ]
 
 MEDIA_URL = '/media/'
